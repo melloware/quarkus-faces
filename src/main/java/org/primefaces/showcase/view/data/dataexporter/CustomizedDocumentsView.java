f52b179a0feda561257f3ddb7e718e4913b3ebcf
melloware <mellowaredev@gmail.com>
2022-09-22 12:28:42 -0400

    Fixed customized exporter

--- src/main/java/org/primefaces/showcase/view/data/dataexporter/CustomizedDocumentsView.java
+++ src/main/java/org/primefaces/showcase/view/data/dataexporter/CustomizedDocumentsView.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2009-2021 PrimeTek
+ * Copyright (c) 2009-2022 PrimeTek
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,15 +23,10 @@
  */
 package org.primefaces.showcase.view.data.dataexporter;
 
-import com.lowagie.text.*;
-import org.apache.poi.hssf.usermodel.*;
-import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.ss.usermodel.FillPatternType;
-import org.primefaces.component.export.ExcelOptions;
-import org.primefaces.component.export.PDFOptions;
-import org.primefaces.component.export.PDFOrientationType;
-import org.primefaces.showcase.domain.Product;
-import org.primefaces.showcase.service.ProductService;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
@@ -39,44 +34,57 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.File;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.List;
+
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.xssf.streaming.SXSSFCell;
+import org.apache.poi.xssf.streaming.SXSSFRow;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.primefaces.component.export.ExcelOptions;
+import org.primefaces.component.export.PDFOptions;
+import org.primefaces.component.export.PDFOrientationType;
+import org.primefaces.showcase.domain.Product;
+import org.primefaces.showcase.service.ProductService;
+
+import com.lowagie.text.*;
 
 @Named
 @RequestScoped
 public class CustomizedDocumentsView implements Serializable {
-    
+
+    private static final long serialVersionUID = 1L;
+
     private List<Product> products;
-    
+
     private List<Product> products2;
-    
+
     private ExcelOptions excelOpt;
-    
+
     private PDFOptions pdfOpt;
-        
+
     @Inject
     ProductService service;
-    
+
     @PostConstruct
     public void init() {
         products = service.getProducts(100);
-        
+
         products2 = service.getProducts(100);
         customizationOptions();
     }
-    
+
     public void customizationOptions() {
         excelOpt = new ExcelOptions();
         excelOpt.setFacetBgColor("#F88017");
         excelOpt.setFacetFontSize("10");
         excelOpt.setFacetFontColor("#0000ff");
         excelOpt.setFacetFontStyle("BOLD");
-        excelOpt.setCellFontColor("#00ff00");
+        excelOpt.setCellFontColor("#145c14");
         excelOpt.setCellFontSize("8");
         excelOpt.setFontName("Verdana");
-        
+
         pdfOpt = new PDFOptions();
         pdfOpt.setFacetBgColor("#F88017");
         pdfOpt.setFacetFontColor("#0000ff");
@@ -113,31 +121,34 @@ public class CustomizedDocumentsView implements Serializable {
     public void setService(ProductService service) {
         this.service = service;
     }
-    
+
     public void postProcessXLS(Object document) {
-		HSSFWorkbook wb = (HSSFWorkbook) document;
-		HSSFSheet sheet = wb.getSheetAt(0);
-		HSSFRow header = sheet.getRow(0);
-		
-		HSSFCellStyle cellStyle = wb.createCellStyle();  
-		cellStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.GREEN.getIndex());
-		cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-		
-		for(int i=0; i < header.getPhysicalNumberOfCells();i++) {
-			HSSFCell cell = header.getCell(i);
-			
-			cell.setCellStyle(cellStyle);
-		}
-	}
-	
-	public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
-		Document pdf = (Document) document;
+        SXSSFWorkbook wb = (SXSSFWorkbook) document;
+        SXSSFSheet sheet = wb.getSheetAt(0);
+        SXSSFRow header = sheet.getRow(0);
+
+        CellStyle cellStyle = wb.createCellStyle();
+        cellStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.GREEN.getIndex());
+        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        for (int i = 0; i < header.getPhysicalNumberOfCells(); i++) {
+            SXSSFCell cell = header.getCell(i);
+
+            cell.setCellStyle(cellStyle);
+        }
+    }
+
+    public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
+        Document pdf = (Document) document;
         pdf.open();
         pdf.setPageSize(PageSize.A4);
 
         ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-        String logo = externalContext.getRealPath("") + File.separator + "resources" + File.separator + "demo" + File.separator + "images" + File.separator + "prime_logo.png";
-		
-		pdf.add(Image.getInstance(logo));
-	}
+
+        String separator = File.separator;
+        String logo = externalContext.getRealPath("") + separator
+                + "resources" + separator + "showcase" + separator + "images" + separator + "primefaces-logo.png";
+
+        pdf.add(Image.getInstance(logo));
+    }
 }
