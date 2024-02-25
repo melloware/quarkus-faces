782b5354719e41db88b7b36f404243e6ee60a647
Marc Baumgartner <marc@mabaka.ch>
2024-02-25 18:05:35 +0100

    Fix package mismatch in TechnicalInfo.java

--- src/main/java/org/primefaces/showcase/view/app/TechnicalInfo.java
+++ src/main/java/org/primefaces/showcase/view/app/TechnicalInfo.java
@@ -19,7 +19,7 @@
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
  */
-package org.primefaces.extensions.showcase.webapp;
+package org.primefaces.showcase.view.app;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
