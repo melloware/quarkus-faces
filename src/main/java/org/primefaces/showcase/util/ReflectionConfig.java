56ff3b3b434dd1d7465caf1cbdd462e7e06904d4
melloware <mellowaredev@gmail.com>
2024-11-25 10:53:45 -0500

    OmniFaces 4.6.1

--- src/main/java/org/primefaces/showcase/util/ReflectionConfig.java
+++
@@ -1,7 +0,0 @@
-package org.primefaces.showcase.util;
-
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-@RegisterForReflection(targets = {org.omnifaces.cdi.push.SocketEndpoint.class}, serialization = true)
-public class ReflectionConfig {
-}
