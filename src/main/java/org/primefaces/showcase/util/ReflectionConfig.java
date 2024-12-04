0d9b49e5aa85b331af26a935f9001a6209d0b803
melloware <mellowaredev@gmail.com>
2024-12-04 14:37:58 -0500

    Added ReflectionConfig

---
+++ src/main/java/org/primefaces/showcase/util/ReflectionConfig.java
@@ -0,0 +1,7 @@
+package org.primefaces.showcase.util;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection(targets = {org.primefaces.util.FacetUtils.class})
+public class ReflectionConfig {
+}
